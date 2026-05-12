0e9646c3d1b54260d7c7ca85db15e455804f7c2a
Lemsito22 <lemquilca22@gmail.com>
2026-05-12 11:20:36 +0200

    Interfaz gráfica

--- Aplicacion_Tiempo/src/main/java/module-info.java
+++ Aplicacion_Tiempo/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module org.example.aplicacion_tiempo {
     requires javafx.fxml;
 
     requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
 
     opens org.example.aplicacion_tiempo to javafx.fxml;
     exports org.example.aplicacion_tiempo;
--- Aplicacion_Tiempo/src/main/java/org/example/aplicacion_tiempo/AplicacionControlador.java
+++ Aplicacion_Tiempo/src/main/java/org/example/aplicacion_tiempo/AplicacionControlador.java
@@ -1,5 +1,19 @@
 package org.example.aplicacion_tiempo;
 
-public class AplicacionControlador {
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
+
+public class AplicacionControlador {
+    @FXML
+    private TextField txtCiudad;
+
+    @FXML
+    private Label lblTemp, lblDescripcion, lblHumedad, lblViento;
+
+    @FXML
+    private void onConsultarClick() {
+        System.out.println("Botón pulsado!");
+    }
 }
