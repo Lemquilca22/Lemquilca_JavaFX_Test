a3a295a3e7ebbac23997cfbec46de1c0eec31cbd
Lemsito22 <lemquilca22@gmail.com>
2026-05-19 10:06:02 +0200

    .

--- PaintChafa/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.paintchafa {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens org.example.paintchafa to javafx.fxml;
-    exports org.example.paintchafa;
-}
